7e2f5b7bc322dbc09cd6e15342c2bd69723d1085
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-04-25 00:53:34 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,38 @@
+// Created by: Mark Palfi
+// Created on: Mar 2022
+//
+// This program does math 
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double salary;
+        double hours;
+        double pay;
+        double taxes;
+
+
+        Console.WriteLine("This program calculates the pay and taxes of the user.");
+        Console.WriteLine("");
+        
+        // input
+        Console.Write("Enter salary: ");
+        salary = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter salary: ");
+        hours = Convert.ToDouble(Console.ReadLine());
+        
+        // process
+        pay = hours * salary * (1.00 - 0.18);
+        taxes = hours * salary * 0.18;
+
+        // output
+        Console.WriteLine("Your pay is = $" + pay + "" );
+        Console.WriteLine("You're taxed = $" + taxes + "");
+
+        Console.WriteLine("\nDone.");
+    }
+}
